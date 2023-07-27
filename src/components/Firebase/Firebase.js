00a611a9124d227b4d9be3272eae9d7351bc0fd3
Vanshita Dev <83915357+vanshitadev10@users.noreply.github.com>
2023-07-27 18:20:32 +0530

    Update Firebase.js
    added firebase

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -2,16 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
+  apiKey: "AIzaSyDrD-pmnug1H_17ppuJYlkOGE1AssRqZqE",
+  authDomain: "expense-tracker-54c98.firebaseapp.com",
+  projectId: "expense-tracker-54c98",
+  storageBucket: "expense-tracker-54c98.appspot.com",
+  messagingSenderId: "594508790458",
+  appId: "1:594508790458:web:aec6208f13aab216e4e083"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
